db6e82be5a582a71ba759b4c6713e1ca2065fb1b
Gustavo <gustavoluigib@gmail.com>
2022-06-11 14:14:43 -0300

    fix for heroku

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('./data/db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build',
-// });
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
 const PORT = process.env.PORT || 3001;
 server.use(middlewares);
-
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
@@ -17,7 +15,6 @@ server.use(
 );
 
 server.use(router);
-
 server.listen(PORT, () => {
   console.log('Server is running');
 });
